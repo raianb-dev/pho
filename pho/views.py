05d4cf7c666bcf0c6065755ce13dbab19f0b4fca
RAIAN BARBOSA <raianpbstudio@gmail.com>
2023-06-15 11:27:31 -0300

    added new redirect

--- pho/views.py
+++ pho/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def pagina_vendas (requests):
-    return render(requests, 'sem-delay.html')
+    return render(requests, 'index.html')
+
 def pagina_vendas_delay (requests):
     return render(requests, 'com-delay.html')
